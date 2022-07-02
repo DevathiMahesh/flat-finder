e656282c7e6f19c0d711808df44f327f7c3bbe10
Devathi Mahesh <d.mahesh995@gmail.com>
2022-07-03 01:25:26 +0530

    Fixed menu issue for login/singup

--- sections/FlatFinderMain.js
+++ sections/FlatFinderMain.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Center, NativeBaseProvider, extendTheme } from 'native-base';
-import Login from './Login/Login';
-import NavBar from './Nav-Bar/nav-bar';
-import FlatCard from '../components/flat-card/flat-card';
+import { Center, NativeBaseProvider, extendTheme } from "native-base";
+import Login from "./Login/Login";
+import NavBar from "./Nav-Bar/nav-bar";
+import FlatCard from "../components/flat-card/flat-card";
+import Profile from "./Profile/profile";
 
 // Define the config
 const config = {
   useSystemColorMode: false,
-  initialColorMode: 'dark',
+  initialColorMode: "dark",
 };
 
 // extend the theme
 export const theme = extendTheme({ config });
 
 export default function FlatFinderMain() {
+  const [activePage, setActivePage] = useState("home");
   return (
     <NativeBaseProvider>
-      <NavBar />
-      <FlatCard />
+      {activePage === "home" && (
+        <>
+          <NavBar setActivePage={setActivePage} />
+          <FlatCard />
+        </>
+      )}
+      {activePage === "profile" && <Profile setActivePage={setActivePage} />}
     </NativeBaseProvider>
   );
 }
